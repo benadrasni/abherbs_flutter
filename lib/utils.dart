bf1b4108b0bce43868339cd40001e294596de6b6
abspglobal <adrian.benko@spglobal.com>
2019-01-31 13:00:47 -0600

    redirect to iap when search

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:abherbs_flutter/enhancements.dart';
 import 'package:abherbs_flutter/purchases.dart';
 import 'package:abherbs_flutter/search/search.dart';
 import 'package:abherbs_flutter/generated/i18n.dart';
@@ -77,22 +78,38 @@ Future<void> launchURLF(String url) {
 
 String getTaxonLabel(BuildContext context, String taxon) {
   switch (taxon) {
-    case 'Superregnum': return S.of(context).taxonomy_superregnum;
-    case 'Regnum': return S.of(context).taxonomy_regnum;
-    case 'Cladus': return S.of(context).taxonomy_cladus;
-    case 'Ordo': return S.of(context).taxonomy_ordo;
-    case 'Familia': return S.of(context).taxonomy_familia;
-    case 'Subfamilia': return S.of(context).taxonomy_subfamilia;
-    case 'Tribus': return S.of(context).taxonomy_tribus;
-    case 'Subtribus': return S.of(context).taxonomy_subtribus;
-    case 'Genus': return S.of(context).taxonomy_genus;
-    case 'Subgenus': return S.of(context).taxonomy_subgenus;
-    case 'Supersectio': return S.of(context).taxonomy_supersectio;
-    case 'Sectio': return S.of(context).taxonomy_sectio;
-    case 'Subsectio': return S.of(context).taxonomy_subsectio;
-    case 'Serie': return S.of(context).taxonomy_serie;
-    case 'Subserie': return S.of(context).taxonomy_subserie;
-    default: return S.of(context).taxonomy_unknown;
+    case 'Superregnum':
+      return S.of(context).taxonomy_superregnum;
+    case 'Regnum':
+      return S.of(context).taxonomy_regnum;
+    case 'Cladus':
+      return S.of(context).taxonomy_cladus;
+    case 'Ordo':
+      return S.of(context).taxonomy_ordo;
+    case 'Familia':
+      return S.of(context).taxonomy_familia;
+    case 'Subfamilia':
+      return S.of(context).taxonomy_subfamilia;
+    case 'Tribus':
+      return S.of(context).taxonomy_tribus;
+    case 'Subtribus':
+      return S.of(context).taxonomy_subtribus;
+    case 'Genus':
+      return S.of(context).taxonomy_genus;
+    case 'Subgenus':
+      return S.of(context).taxonomy_subgenus;
+    case 'Supersectio':
+      return S.of(context).taxonomy_supersectio;
+    case 'Sectio':
+      return S.of(context).taxonomy_sectio;
+    case 'Subsectio':
+      return S.of(context).taxonomy_subsectio;
+    case 'Serie':
+      return S.of(context).taxonomy_serie;
+    case 'Subserie':
+      return S.of(context).taxonomy_subserie;
+    default:
+      return S.of(context).taxonomy_unknown;
   }
 }
 
@@ -103,7 +120,8 @@ String getProductTitle(BuildContext context, String productId, String defaultTit
       return S.of(context).product_no_ads_title;
     case productSearch:
       return S.of(context).product_search_title;
-    default: return defaultTitle;
+    default:
+      return defaultTitle;
   }
 }
 
@@ -114,7 +132,8 @@ String getProductDescription(BuildContext context, String productId, String defa
       return S.of(context).product_no_ads_description;
     case productSearch:
       return S.of(context).product_search_description;
-    default: return defaultDescription;
+    default:
+      return defaultDescription;
   }
 }
 
@@ -127,17 +146,22 @@ Icon getIcon(String productId) {
 
 List<Widget> getActions(BuildContext context, Function(String) onChangeLanguage, Function() onBuyProduct) {
   var _actions = <Widget>[];
-  if (Purchases.isSearch()) {
-    _actions.add(IconButton(
-      icon: getIcon(productSearch),
-      onPressed: () {
+  _actions.add(IconButton(
+    icon: getIcon(productSearch),
+    onPressed: () {
+      if (Purchases.isSearch()) {
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => Search(Localizations.localeOf(context), onChangeLanguage, onBuyProduct)),
         );
-      },
-    ));
-  }
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EnhacementsScreen(onChangeLanguage, onBuyProduct)),
+        );
+      }
+    },
+  ));
 
   return _actions;
 }
