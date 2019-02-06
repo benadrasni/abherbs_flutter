3c750b63e05cf7abb14bc531848a42157c049b15
benadrasni <adrian.benko@gmail.com>
2019-02-05 19:10:40 -0600

    offline product defined

--- lib/purchases.dart
+++ lib/purchases.dart
@@ -6,6 +6,15 @@ class Purchases {
   static bool isAllowed = false;
   static List<PurchasedItem> purchases = <PurchasedItem>[];
 
+  static bool isPurchased(String productId) {
+    for (var purchase in purchases) {
+      if (purchase.productId == productId) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   static bool isNoAds() {
     for (PurchasedItem product in purchases) {
       if (product.productId == productNoAdsAndroid || product.productId == productNoAdsIOS) {
@@ -27,6 +36,15 @@ class Purchases {
   static bool isCustomFilter() {
     for (PurchasedItem product in purchases) {
       if (product.productId == productCustomFilter) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static bool isOffline() {
+    for (PurchasedItem product in purchases) {
+      if (product.productId == productOffline) {
         return true;
       }
     }
