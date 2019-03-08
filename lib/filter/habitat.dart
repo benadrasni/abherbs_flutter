30cdecaea425bc79501f8f238e59ddd1666f6f07
benadrasni <adrian.benko@gmail.com>
2019-03-07 20:35:11 -0600

    scheduler

--- lib/filter/habitat.dart
+++ lib/filter/habitat.dart
@@ -12,6 +12,7 @@ import 'package:abherbs_flutter/utils/utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_inapp_purchase/flutter_inapp_purchase.dart';
 
 class Habitat extends StatefulWidget {
@@ -34,11 +35,12 @@ class _HabitatState extends State<Habitat> {
   bool _wasRedirected;
 
   _redirect(BuildContext context) async {
-    var _duration = Duration(milliseconds: timer);
-    var redirectRoute = await widget.redirect;
-    return Timer(_duration, () {
+    // redirect to route from notification
+    if (widget.redirect != null && !_wasRedirected) {
+      _wasRedirected = true;
+      var redirectRoute = await widget.redirect;
       Navigator.push(context, redirectRoute);
-    });
+    }
   }
 
   _navigate(String value) {
@@ -96,6 +98,8 @@ class _HabitatState extends State<Habitat> {
     if (widget.redirect == null) {
       Ads.showBannerAd(this);
     }
+
+    SchedulerBinding.instance.addPostFrameCallback((_) => _redirect(context));
   }
 
   @override
@@ -112,13 +116,6 @@ class _HabitatState extends State<Habitat> {
       fontSize: 22.0,
       color: Colors.white,
     );
-
-    // redirect to route from notification
-    if (widget.redirect != null && !_wasRedirected) {
-      _redirect(context);
-      _wasRedirected = true;
-    }
-
     return Scaffold(
       key: _key,
       appBar: new AppBar(
