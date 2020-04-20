dbc61ffc7fece8a938b2790724a2d707a3f62989
benadrasni <adrian.benko@gmail.com>
2020-04-19 19:08:23 -0500

    5.8.10

--- android/app/src/main/kotlin/sk/ab/herbs/MainActivity.kt
+++ android/app/src/main/kotlin/sk/ab/herbs/MainActivity.kt
@@ -1,6 +1,13 @@
 package sk.ab.herbs
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
 }
